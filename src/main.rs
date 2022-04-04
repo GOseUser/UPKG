5478978033a57e0311db8128c3f5acd4d2604dc3
Demir Yerli <mrquantumoff@protonmail.com>
2022-04-04 17:54:01 +0300

    Start working on frontend (at least somehow)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
 use std::env;
 use upkg;
 fn main() {
-    
+    let args: Vec<String> = env::args().collect();
+    for arg in args{
+        if arg == "-h" || arg == "--help"{
+            help();
+        }
+        if arg == "-v" || arg == "--version"{
+            version();
+        }
+    }
+}
+fn help() {
+    println!("Usage: upkg [options] <package>");
+    println!("Options:");
+    println!("  -h, --help    Show this help message");
+    println!("  -v, --version Show libupkg version");
+    return;
+}
+fn version() {
+    println!("upkg {}", upkg::VERSION);
+    return;
 }
